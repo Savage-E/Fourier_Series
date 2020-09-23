5ecc1412db570dc2bdef3abe4edde35cf4322771
Vladislav Kotov <vlad.kotov1999@ya.ru>
2020-09-23 23:46:14 +0300

    created 3 graphs

---
+++ FourierSeries/WindowsFormsApp1/MainForm.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._radioBtnX = new System.Windows.Forms.RadioButton();
+            this._radioBtnX2 = new System.Windows.Forms.RadioButton();
+            this._radioBtnSgn = new System.Windows.Forms.RadioButton();
+            this._textBoxN = new System.Windows.Forms.TextBox();
+            this._lblInputN = new System.Windows.Forms.Label();
+            this._BtnRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // _radioBtnX
+            // 
+            this._radioBtnX.AutoSize = true;
+            this._radioBtnX.Location = new System.Drawing.Point(547, 12);
+            this._radioBtnX.Name = "_radioBtnX";
+            this._radioBtnX.Size = new System.Drawing.Size(50, 17);
+            this._radioBtnX.TabIndex = 0;
+            this._radioBtnX.TabStop = true;
+            this._radioBtnX.Text = "f(x)=x";
+            this._radioBtnX.UseVisualStyleBackColor = true;
+            // 
+            // _radioBtnX2
+            // 
+            this._radioBtnX2.AutoSize = true;
+            this._radioBtnX2.Location = new System.Drawing.Point(547, 35);
+            this._radioBtnX2.Name = "_radioBtnX2";
+            this._radioBtnX2.Size = new System.Drawing.Size(62, 17);
+            this._radioBtnX2.TabIndex = 1;
+            this._radioBtnX2.TabStop = true;
+            this._radioBtnX2.Text = "f(x)=x^2";
+            this._radioBtnX2.UseVisualStyleBackColor = true;
+            // 
+            // _radioBtnSgn
+            // 
+            this._radioBtnSgn.AutoSize = true;
+            this._radioBtnSgn.Location = new System.Drawing.Point(547, 58);
+            this._radioBtnSgn.Name = "_radioBtnSgn";
+            this._radioBtnSgn.Size = new System.Drawing.Size(73, 17);
+            this._radioBtnSgn.TabIndex = 2;
+            this._radioBtnSgn.TabStop = true;
+            this._radioBtnSgn.Text = "f(x)=sgn(x)";
+            this._radioBtnSgn.UseVisualStyleBackColor = true;
+            // 
+            // _textBoxN
+            // 
+            this._textBoxN.Location = new System.Drawing.Point(547, 94);
+            this._textBoxN.Name = "_textBoxN";
+            this._textBoxN.Size = new System.Drawing.Size(89, 20);
+            this._textBoxN.TabIndex = 3;
+            this._textBoxN.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this._textBoxN_KeyPress);
+            // 
+            // _lblInputN
+            // 
+            this._lblInputN.AutoSize = true;
+            this._lblInputN.Location = new System.Drawing.Point(544, 78);
+            this._lblInputN.Name = "_lblInputN";
+            this._lblInputN.Size = new System.Drawing.Size(92, 13);
+            this._lblInputN.TabIndex = 4;
+            this._lblInputN.Text = "Введите число N";
+            // 
+            // _BtnRun
+            // 
+            this._BtnRun.Location = new System.Drawing.Point(547, 120);
+            this._BtnRun.Name = "_BtnRun";
+            this._BtnRun.Size = new System.Drawing.Size(75, 23);
+            this._BtnRun.TabIndex = 5;
+            this._BtnRun.Text = "Выполнить";
+            this._BtnRun.UseVisualStyleBackColor = true;
+            this._BtnRun.Click += new System.EventHandler(this._BtnRun_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 462);
+            this.Controls.Add(this._BtnRun);
+            this.Controls.Add(this._lblInputN);
+            this.Controls.Add(this._textBoxN);
+            this.Controls.Add(this._radioBtnSgn);
+            this.Controls.Add(this._radioBtnX2);
+            this.Controls.Add(this._radioBtnX);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.Text = "Ряды Фурье";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton _radioBtnX;
+        private System.Windows.Forms.RadioButton _radioBtnX2;
+        private System.Windows.Forms.RadioButton _radioBtnSgn;
+        private System.Windows.Forms.TextBox _textBoxN;
+        private System.Windows.Forms.Label _lblInputN;
+        private System.Windows.Forms.Button _BtnRun;
+    }
+}
+
---
+++ FourierSeries/WindowsFormsApp1/MainForm.cs
@@ -0,0 +1,158 @@
+﻿using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.WindowsForms;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class MainForm : Form
+    {
+        public PlotView pv = new PlotView
+        {
+            Location = new Point(0, 0),
+            Size = new Size(550, 450),
+            Model = new PlotModel { Title = "Graphs" },
+        };
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Controls.Add(pv);
+        }
+
+        private void _textBoxN_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != Convert.ToChar(8))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void _BtnRun_Click(object sender, EventArgs e)
+        {
+            FunctionSeries functionSeries1;
+            FunctionSeries functionSeries2;
+            int N;
+            if (_radioBtnX.Checked)
+            {
+                functionSeries1 = new FunctionSeries();
+                functionSeries2 = new FunctionSeries();
+                functionSeries1.Title = "SumX";
+                functionSeries2.Title = "X";
+                functionSeries1.MarkerStroke = OxyColors.Red;
+                functionSeries2.MarkerStroke = OxyColors.Bisque;
+
+                pv.Model = new PlotModel { Title = "f(x)=x" };
+                pv.Model.Series.Add(functionSeries1);
+                pv.Model.Series.Add(functionSeries2);
+
+                 N = int.Parse(_textBoxN.Text);
+
+                for (double i = -4; i <= 4; i += 0.1)
+                {
+                    functionSeries1.Points.Add(new DataPoint(i, SumX(i, N)));
+                    functionSeries2.Points.Add(new DataPoint(i, X(i)));
+                }
+            }
+            if (_radioBtnX2.Checked)
+            {
+                functionSeries1 = new FunctionSeries();
+                functionSeries2 = new FunctionSeries();
+                functionSeries1.Title = "Sum(X^2)";
+                functionSeries2.Title = "X^2";
+                functionSeries1.MarkerStroke = OxyColors.Red;
+                functionSeries2.MarkerStroke = OxyColors.Orange;
+
+                pv.Model = new PlotModel { Title = "f(x)=x^2" };
+                pv.Model.Series.Add(functionSeries1);
+                pv.Model.Series.Add(functionSeries2);
+
+                N = int.Parse(_textBoxN.Text);
+
+                for (double i = -4; i <= 4; i += 0.1)
+                {
+                    functionSeries1.Points.Add(new DataPoint(i, SumX2(i, N)));
+                    functionSeries2.Points.Add(new DataPoint(i, X2(i)));
+                }
+            }
+            if (_radioBtnSgn.Checked)
+            {
+                functionSeries1 = new FunctionSeries();
+                functionSeries2 = new FunctionSeries();
+                functionSeries1.Title = "Sum Sign";
+                functionSeries2.Title = "Sign";
+                functionSeries1.MarkerStroke = OxyColors.Yellow;
+                functionSeries2.MarkerStroke = OxyColors.Green;
+
+                pv.Model = new PlotModel { Title = "Sign x" };
+                pv.Model.Series.Add(functionSeries1);
+                pv.Model.Series.Add(functionSeries2);
+
+                N = int.Parse(_textBoxN.Text);
+
+                for (double i = -4; i <= 4; i += 0.1)
+                {
+                    functionSeries1.Points.Add(new DataPoint(i, SumSgn(i, N)));
+                    functionSeries2.Points.Add(new DataPoint(i, Sgn(i)));
+                }
+
+            }
+        }
+
+        public double X(double x)
+        {
+            return x;
+        }
+
+        public double SumX(double x, int N)
+        {
+            double sum = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                sum += ((Math.Pow(-1, i + 1) * Math.Sin(i * x)) / i);
+            }
+            return 2 * sum;
+        }
+
+        public double X2(double x)
+        {
+            return x *x;
+        }
+
+        public double SumX2(double x, int N)
+        {
+            double sum = 0;
+
+            for (int i = 1; i <= N; i++)
+            {
+                sum += (Math.Pow(-1, i) / Math.Cos(i * x)) / i * i;
+            }
+
+            return ((Math.PI * Math.PI) / 2) + 4 * sum;
+        }
+
+
+        public double Sgn(double x)
+        {
+            return Math.Sign(x);
+        }
+
+        public double SumSgn(double x, int N)
+        {
+            double sum = 0;
+
+            for (int i = 1; i <= N; i++)
+            {
+                sum += (Math.Sin((2*i-1)*x)) / (2*i -1);
+            }
+
+            return (4 / Math.PI) * sum;
+        }
+    }
+}
